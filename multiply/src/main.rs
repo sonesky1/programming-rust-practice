679681874ada750aa82723d0fcefab520f67a5ef
sonesky1 <susan.onesky@gmail.com>
2025-02-06 17:46:08 -0800

    update

--- multiply/src/main.rs
+++ multiply/src/main.rs
@@ -1,45 +1,35 @@
 //Program to multiply all numbers given on command line starting with first number input
 
-use std::str::FromStr;
 use std::env;
+use std::str::FromStr;
 
 fn main() {
-   let mut nums = Vec::new();
+    let mut nums = Vec::new();
 
-   for arg in env::args().skip(1){
-      nums.push(u64::from_str(&arg).expect("error parsing argument")); //what is this?
-    
-   }
-   if nums.len() == 0 {
-     eprintln!("Usage: multiply ...");
-     std::process::exit(1);
-   }
+    for arg in env::args().skip(1) {
+        nums.push(u64::from_str(&arg).expect("error parsing argument")); //what is this?
+    }
+    if nums.len() == 0 {
+        eprintln!("Usage: multiply ...");
+        std::process::exit(1);
+    }
 
-   let mut d = nums[0];
-   for m in &nums[1..] {
-     d = multiply(d, *m);
-  }
+    let mut d = nums[0];
+    for m in &nums[1..] {
+        d = multiply(d, *m);
+    }
 
-   
-   
     println!("All the numbers multiplied {:?} is {}", nums, d);
-
-
 }
 //multiply 2 numbers
 fn multiply(n: u64, m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
 
-   assert!(n != 0 && m != 0);
-
-   let p = m * n;
-   p
-  
-
-
+    let p = m * n;
+    p
 }
 
 #[test]
-fn test_multiply(){
+fn test_multiply() {
     assert_eq!(multiply(6, 1), 6);
-
 }
